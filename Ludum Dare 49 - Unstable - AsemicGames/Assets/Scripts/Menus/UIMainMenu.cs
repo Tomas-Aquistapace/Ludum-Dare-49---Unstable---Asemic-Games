15c1d3344fc75272a003c7e88b711e39732f6f00
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2021-10-02 15:20:05 -0300

    Creado main menu

---
+++ Ludum Dare 49 - Unstable - AsemicGames/Assets/Scripts/Menus/UIMainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIMainMenu : MonoBehaviour
+{
+    public void GoToGameplayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
